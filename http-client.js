219cbf19bb49bbb9bdf4fce7e77ae75070d1d923
Chris Barton <c.barton@adelaide.edu.au>
2016-03-08 16:58:10 +1030

    http client done

---
+++ http-client.js
@@ -0,0 +1,15 @@
+var http = require('http');
+
+var path = process.argv[2];
+
+var request = http.get(path, function(response) {
+    // set the encoding to string
+    response.setEncoding('utf8');
+    response.on('data', function(d) {
+      console.log(d);
+    });
+});
+
+request.on('error', function(e) {
+  console.log('problem with request: ' + e.message);
+});
